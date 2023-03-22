7597b051d1d1d82251b996e3ad35855d724a80fa
AturiheihiBlendon <2020blendon@gmail.com>
2023-03-22 21:50:33 +0300

    Readfile with Asynch

--- filesystem/file.js
+++ filesystem/file.js
@@ -1,10 +1,22 @@
 const fs = require('fs')
 
-const content = fs.readFileSync('./filesystem/lorem.txt','utf8')
-console.log(content)
+// Readfile Sync
 
-console.log("******************************************************");
+// const content = fs.readFileSync('./filesystem/lorem.txt','utf8')
+// console.log(content)
 
-const data = "Hello I am greeting you in a new file"
-const new_content = fs.writeFileSync('./filesystem/greet.txt',`${data}`)
-console.log(fs.readFileSync('./filesystem/greet.txt','utf8'));
+// console.log("******************************************************");
+
+// const data = "Hello I am greeting you in a new file"
+// const new_content = fs.writeFileSync('./filesystem/greet.txt',`${data}`)
+// console.log(fs.readFileSync('./filesystem/greet.txt','utf8'));
+
+
+// Readfile Asynch
+
+const content = fs.readFile('./filesystem/lorem.txt', 'utf8', (err, data) => {
+    if (err){
+        console.log(err)
+    }
+    console.log(data)
+})
