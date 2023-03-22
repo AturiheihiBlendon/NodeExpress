7a1752ea17c596893dbc57e223c852a19d8280c3
AturiheihiBlendon <2020blendon@gmail.com>
2023-03-22 08:29:44 +0300

    Global variables in node

---
+++ global.js
@@ -0,0 +1,19 @@
+// __dirname - returns current working directory
+// __filename - returns the filename
+// module
+// require
+// setTimeout
+// setInterval
+
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(module)
+console.log(require)
+
+setInterval(() => {
+    console.log("Hello world");
+}, 1000);
+
+setTimeout(() => {
+    console.log("Hi, there?");
+}, 5000);
